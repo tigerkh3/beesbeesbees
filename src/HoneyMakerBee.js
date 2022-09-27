d5118981661b6a2e83cc7114386e08bf0caaab2c
Tiger Hong <tigerhong186@gmail.com>
2022-09-26 19:44:09 -0700

    "Complete HoneyMakerBee"

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+  // call the Bee superclass
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+};
+
+// set the prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+// set the constructor
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot += 1;
+};
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot -= 1;
 };
 
