1f6742d574fa591c43915431d8fa23efe6413a7c
Tiger Hong <tigerhong186@gmail.com>
2022-09-26 19:56:32 -0700

    "Complete ForagerBee"

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,25 @@
 var ForagerBee = function() {
+  // call the Bee superclass
+  HoneyMakerBee.call(this);
+  // new job property
+  this.job = 'find pollen';
+  // new canFly property
+  this.canFly = true;
+  // new treasureChest property
+  this.treasureChest = [];
 };
+
+// set the prototype
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+// set the constructor
+ForagerBee.prototype.constructor = ForagerBee;
+
+// new forage method that allows bee to add treasure to treasureChest
+ForagerBee.prototype.forage = function (treasure) {
+  // we want to just add treasure into the array
+  // just push it into the array
+  this.treasureChest.push(treasure);
+}
+
+
 
