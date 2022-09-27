919f65fce4c13d08653b676072d928eff0bbb37e
Tiger Hong <tigerhong186@gmail.com>
2022-09-26 19:38:46 -0700

    "Complete Bee"

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,17 @@
 var Bee = function() {
+  // we want to call on the Grub function
+  // sets the prototype in our understanding
+  Grub.call(this);
+  // set age property to be 5
+  this.age = 5;
+  // set color property
+  this.color = 'yellow';
+  // set job property as 'keep on growing'
+  this.job = 'keep on growing';
 };
 
+// setting the prototype by delegating object to Grub prototype
+// this allows us to access Grub + it's methods (maybe)?
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.constructor = Bee;
