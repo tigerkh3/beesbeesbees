ed72d8f153cf27ce8ba32ca23a9f164c87f72916
Tiger Hong <tigerhong186@gmail.com>
2022-09-26 19:56:49 -0700

    "Complete Bare Minimum Requirements"

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,23 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  // new age property
+  this.age = 40;
+  // new job property
+  this.job = 'gamble';
+  // new color property
+  this.color = 'grey';
+  // new canFly property
+  this.canFly = false;
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
